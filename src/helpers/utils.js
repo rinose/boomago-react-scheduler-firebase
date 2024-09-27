380c5c6424c84c35c2485af4b3fc39b344a1eac6
rinose <rinosetola@gmail.com>
2024-09-27 17:11:48 +0200

    removed twilo account

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -2,8 +2,8 @@ import { LocalStoragesKeys } from '../config/constants';
 
 
 
-const TWILIO_ACCOUNT_SID = "AC6c491b13ba7911d1cfa347770412e267";
-const TWILIO_AUTH_TOKEN = "af9058648d853d0aa65a63fb54c7b302";//process.env.TWILIO_AUTH_TOKEN;
+const TWILIO_ACCOUNT_SID = "";
+const TWILIO_AUTH_TOKEN = "";//process.env.TWILIO_AUTH_TOKEN;
 
 
 export const sendScheduledMessage = (body, to, send_at) => {
